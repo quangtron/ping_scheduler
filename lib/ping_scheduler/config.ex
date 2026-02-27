53349d1e26bf5982079819c764c8a8597c859ddc
quangtron <haquangtrong.ton@gmail.com>
2026-02-27 17:27:58 +0700

    update config

--- lib/ping_scheduler/config.ex
+++ lib/ping_scheduler/config.ex
@@ -7,8 +7,8 @@ defmodule PingScheduler.Config do
     case YamlElixir.read_from_file(config_path) do
       {:ok, config} ->
         api_key = get_config(config, "api_key", System.get_env("ANTHROPIC_API_KEY"))
-        bot_token = get_config(config, "bot_token", System.get_env("BOT_TOKEN"))
-        chat_id = get_config(config, "chat_id", System.get_env("CHAT_ID"))
+        bot_token = get_config(config, ["telegram", "bot_token"], System.get_env("BOT_TOKEN"))
+        chat_id = get_config(config, ["telegram", "chat_id"], System.get_env("CHAT_ID"))
 
         {:ok, Map.merge(config, %{api_key: api_key, bot_token: bot_token, chat_id: chat_id})}
 
@@ -17,12 +17,15 @@ defmodule PingScheduler.Config do
     end
   end
 
-  defp get_config(config, field_name, env_value) do
-    config
-    |> Map.get(field_name)
+  defp get_config(config, field_names, env_value) do
+    if is_list(field_names) do
+      get_in(config, field_names)
+    else
+      Map.get(config, field_names)
+    end
     |> then(fn
-      nil -> env_value
-      key -> key
+      value when value in [nil, ""] -> env_value
+      value -> value
     end)
   end
 
